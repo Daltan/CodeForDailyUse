f5383b5648300f8fb9ff728b2fd00a8faec200e9
Daltan <daltan@foxmail.com>
2018-01-13 21:11:17 +0800

    add code commenting

--- CreateTextFiles/MakeFile.py
+++ CreateTextFiles/MakeFile.py
@@ -1,15 +1,19 @@
 import os
 print('The current directory is：',os.getcwd())
 fileListName='filelist.txt'
+#Get the file list in current directory
 currentFileList=os.listdir()
+#Test wether there is a filelist.txt file
 if fileListName in currentFileList:
     f=open(fileListName,'r')
     fileliststr=f.readline()
     filelist=fileliststr.split(" ")
     for i in filelist:
+        # Create a file whose name is from 'filelist.txt'
         f1=open(i,'w+')
         f1.close()
     print("success creating files in 'filelist.txt'")
+# no filelist.txt file is found
 else:
     noteWords="please create a 'filelist.txt' which contains filename.suffix and space"
     print(noteWords)
