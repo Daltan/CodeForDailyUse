f70d60e6e89844b7885eb5c37b960430b0b83a8e
Daltan <daltan@foxmail.com>
2018-01-13 22:42:32 +0800

    varibles name modified, commenting modified.

--- CreateDirectory/MakeDirectory.py
+++ CreateDirectory/MakeDirectory.py
@@ -1,18 +1,18 @@
 import os
 print('The current directory is：',os.getcwd())
 DirListName='dirlist.txt'
-#Get the file list in current directory
+#Get the directory list in current directory
 currentFileList=os.listdir()
-#Test wether there is a filelist.txt file
+#Test wether there is a dirlist.txt file
 if DirListName in currentFileList:
     f=open(DirListName,'r')
-    fileliststr=f.read()
+    dirliststr=f.read()
     f.close()
-    filelist=fileliststr.split("\n")
-    for i in filelist:
+    dirlist=dirliststr.split("\n")
+    for i in dirlist:
         os.mkdir(i)
     print("success creating directories in dirlist")
-# no filelist.txt file is found
+# no dirlist.txt file is found
 else:
     noteWords="please create a 'dirlist.txt' which contains directory name, each name should be in one line"
     print(noteWords)
